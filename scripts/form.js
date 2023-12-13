c68df9d967828ca3f19f1c28749679b6177577a0
Nathan Li <lio2118152147@gmail.com>
2023-12-13 21:18:24 +0800

    login-build

---
+++ scripts/form.js
@@ -0,0 +1,26 @@
+function getCookie(name) {
+    let cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        let parts = cookies[i].split('=');
+        if (parts[0] === name) {
+            return parts[1];
+        }
+    }
+    return '';
+}
+
+function checkLogin() {
+    let isLoggedIn = (getCookie('loggedIn') === 'true');
+    // 检查本地存储中的登录状态
+    if (!isLoggedIn) {
+        // 如果未登录，重定向到登录页面
+        window.location.href = 'login.html'; // 将 'login.html' 替换为你的登录页面URL
+    } else {
+        // 用户已登录，可以继续留在当前页面或执行其他操作
+        console.log('User is logged in.');
+    }
+}
+
+console.log('Current Cookies:', document.cookie);
+// 页面加载时调用
+checkLogin();
---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // 模拟用户验证
+    if (username === '123' && password === '123') {
+        // 存储用户登录状态
+        document.cookie = "loggedIn=true; max-age=3600; path=/";
+        alert('Login successful');
+        window.location.href = 'form.html';
+    } else {
+        alert('Invalid credentials');
+    }
+});
